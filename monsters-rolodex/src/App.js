62277a275a36c500e1bc8be03cc1817664a06b1e
loganmulvihill <mulvihill.logan@gmail.com>
2020-08-14 08:58:42 -0500

    destructured state and created filteredMonsters variable to filter search

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -21,10 +21,17 @@ class App extends React.Component {
   }
 
   render() {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return (
       <div className="App">
-        <input type='search' placeholder="search monsters" onChange={e => this.setState({searchField: e.target.value})} />
-        <CardList monsters={this.state.monsters} />
+        <input type='search'
+         placeholder="search monsters"
+          onChange={e => this.setState({searchField: e.target.value})} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
