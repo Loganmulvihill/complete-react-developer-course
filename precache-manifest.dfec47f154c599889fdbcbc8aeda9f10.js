de9a001f9fd6da2df56cfac655e578e8e023692e
loganmulvihill <mulvihill.logan@gmail.com>
2020-08-14 22:15:47 -0500

    Updates

---
+++ precache-manifest.dfec47f154c599889fdbcbc8aeda9f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b40c1ed1923cadba3cafa045df807c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9058f6234c1f91c6a854",
+    "url": "/monsters-rolodex/static/css/main.141d7862.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9058f6234c1f91c6a854",
+    "url": "/monsters-rolodex/static/js/main.81e5d1a3.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
