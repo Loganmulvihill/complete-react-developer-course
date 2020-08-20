1256446e66875724c5e13a4bdc301485bad30dbb
loganmulvihill <mulvihill.logan@gmail.com>
2020-08-19 23:06:45 -0500

    added homepage component and sass npm package to file

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -11,10 +11,10 @@ class App extends React.Component {
     super(props);
     this.state = {
       monsters: [],
-      searchField:''
+      searchField: ''
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   componentDidMount() {
@@ -23,28 +23,28 @@ class App extends React.Component {
       .then(users => this.setState({ monsters: users }));
   }
 
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value })
-  }
+  onSearchChange = (e) => {
+    this.setState({searchField: e.target.value})
+  };
 
   render() {
 
-    const { monsters, searchField } = this.state;
+    const { monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
       )
     return (
       <div className="App">
-        <h1> Monsters Rolodex</h1>
+        <h1> Monsters Rolodex </h1>
         <SearchBox
-        placeholder="search monsters"
-        handleChange={this.handleChange}
+          placeholder="search monsters"
+          onSearchChange={this.onSearchChange}
         />
-        <CardList monsters={filteredMonsters}
+        <CardList monsters={monsters}
         />
       </div>
     );
-  }
+ }
 }
 
-export default App;
+  export default App;
